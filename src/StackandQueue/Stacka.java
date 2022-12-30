0a26c87d8c58d84a3955e5478f7c758beda31535
Atulsak <sakhareat@gmail.com>
2022-12-31 00:13:51 +0530

    ADDED: checking

--- src/StackandQueue/Stacka.java
+++ src/StackandQueue/Stacka.java
@@ -5,26 +5,42 @@ import java.util.LinkedList;
 
 
 public class Stacka {
-	  static LinkedList<String> ll = new LinkedList<>();
+	 static LinkedList<String> ll = new LinkedList<>();
 
 	    public static void push(String data){
 	        ll.add(data);
 	    }
 
+	    public static void pop(){
+	        ll.removeLast();
+	    }
 	    public static void print(){
-	        for(String element : ll){
-	            System.out.println("| " + element + "|");
-	            System.out.println(" --- ");
-	        }
-	        System.out.println();
+	        if(ll.size() != 0) {
+	            for (String element : ll) {
+	                System.out.println("| " + element + "|");
+	                System.out.println(" --- ");
+	            }
+	            System.out.println();
+	        } else
+	            System.out.println("Stack is Null.............");
 	    }
 
 	    public static void main(String[] args) {
-	        System.out.println("Welcome to Stack and Queue Problems...");
+	        System.out.println("Welcome to Stack and Queue Problems.");
 	        push("56");
 	        push("30");
 	        push("70");
+	        push("70");
+	        push("70");
+	        push("70");
+	        System.out.println("Stack ::::::::::::::");
+	        print();
+	        int size = ll.size();
+	        System.out.println("size " +ll.size());
+	        for (int i = 0; i < size; i++){
+	            pop();
+	        }
+	        System.out.println("Stack :::::::::::::::::::");
 	        print();
 	    }
-
 }
